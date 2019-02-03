70483176b0a152c91dfaacbc7295e6daba7c7cd0
Sergio Morchón Poveda <sergio.morchon@outlook.com>
2019-02-03 19:03:07 +0100

    Unit preferences

--- app/models/point.ts
+++ app/models/point.ts
@@ -1,15 +1,32 @@
+import { units } from "user-settings";
+import i18n from "../utils/i18n";
+
 export type Point = {
   readonly latitude: number;
   readonly longitude: number;
 };
 
+const { distance: distanceUnits } = units;
 const EARTH_RADIUS = 6371e3;
-const KILOMETER = 1e3;
+const distanceConfig = {
+  metric: {
+    short: 1,
+    shortName: i18n("meter"),
+    long: 1e3,
+    longName: i18n("kilometer")
+  },
+  us: {
+    short: 0.3048,
+    shortName: i18n("foot"),
+    long: 1609.344,
+    longName: i18n("mile")
+  }
+};
+
+const degreesToRadians = (degrees: number) => (degrees * Math.PI) / 180;
 
 export const pointToString = ({ latitude, longitude }: Point) =>
   `${latitude.toFixed(6)}, ${longitude.toFixed(6)}`;
-
-const degreesToRadians = (degrees: number) => (degrees * Math.PI) / 180;
 
 export const getDistance = (from: Point, to: Point) => {
   const latitudeDistance = degreesToRadians(to.latitude - from.latitude);
@@ -23,7 +40,11 @@ export const getDistance = (from: Point, to: Point) => {
   return EARTH_RADIUS * 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 };
 
-export const distanceToString = (distance: ReturnType<typeof getDistance>) =>
-  distance >= KILOMETER
-    ? `${(distance / KILOMETER).toFixed(1)}km`
-    : `${distance.toFixed(0)}m`;
+export const distanceToString = (distance: ReturnType<typeof getDistance>) => {
+  const { short, shortName, long, longName } = distanceConfig[distanceUnits];
+  const longPart = Math.floor(distance / long);
+  const shortPart = Math.floor((distance - longPart * long) / short);
+  return `${
+    longPart ? `${longPart}${longName}` : ""
+  } ${shortPart}${shortName}`.trim();
+};
