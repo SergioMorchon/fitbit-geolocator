e0a1e58ef57f77648b823fb4066dd8371751dda1
Sergio Morchón Poveda <sergio.morchon@outlook.com>
2019-03-02 21:08:25 +0100

    Log memory usage during lifecycle

--- app/data-sources/state.ts
+++ app/data-sources/state.ts
@@ -1,7 +1,6 @@
 import { me } from 'appbit';
 import { readFileSync, writeFileSync } from 'fs';
 import { configureStore } from 'reduced-state';
-import { memory } from 'system';
 import reducers from '../reducers';
 
 const SETTINGS_FILE_NAME = 'storage';
@@ -28,15 +27,6 @@ const originalDispatch = store.dispatch;
 store.dispatch = (action: any) => {
 	// tslint:disable-next-line: no-console
 	console.log(`Action: ${action.type}`);
-	const {
-		js: { used, total },
-	} = memory;
-	// tslint:disable-next-line: no-console
-	console.log(
-		`Memory usage: ${Number((used * 100) / total).toFixed(
-			2,
-		)}% (${used}/${total})`,
-	);
 	originalDispatch(action);
 };
 
--- app/index.ts
+++ app/index.ts
@@ -1,5 +1,7 @@
 import { me } from 'appbit';
+import { memory } from 'system';
 import { LOCATION_SLOTS_VIEW, NAVIGATION_VIEW } from './constants/views';
+import store from './data-sources/state';
 import { createViewSet, INavigation } from './utils/views';
 import { createLocationDetailsView } from './views/location-details-view';
 import { createLocationSlotsView } from './views/location-slots-view';
@@ -18,3 +20,21 @@ views.addView(createNewLocationView(navigation));
 views.addView(createLocationDetailsView(navigation));
 views.addView(createNavigationView(navigation));
 navigation.navigate(LOCATION_SLOTS_VIEW);
+
+const memoryConsumptionToString = (consumed: number, total: number) =>
+	`${Number((consumed * 100) / total).toFixed(2)}% (${consumed}/${total})`;
+
+const logMemory = () => {
+	const {
+		js: { used, total, peak },
+	} = memory;
+	// tslint:disable-next-line: no-console
+	console.log(
+		`Current memory: ${memoryConsumptionToString(used, total)}`,
+		`Peak memory: ${memoryConsumptionToString(peak, total)}`,
+	);
+};
+
+logMemory();
+store.subscribe(logMemory);
+me.addEventListener('unload', logMemory);
